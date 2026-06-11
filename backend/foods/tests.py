fd2eb4488fb98d62f138ad851bd593a2529157f0
rifana <rifanasherin547@gmail.com>
2026-06-11 11:34:00 +0530

    test: add initial pytest setup

--- backend/foods/tests.py
+++ backend/foods/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+class UserTest(TestCase):
+
+    def test_user_creation(self):
+
+        user = User.objects.create(
+            username="test@test.com"
+        )
+
+        self.assertEqual(
+            user.username,
+            "test@test.com"
+        )
